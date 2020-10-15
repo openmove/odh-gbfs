82d460b82db3de6842cf92c49dc86b477212a35f
stemove <31170657+stemove@users.noreply.github.com>
2020-10-15 21:04:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,13 +12,10 @@ const tmplReg = /\$\{([\w_\-]+)\}/g
 //const tmplReg = /\{ *([\w_\-]+) *\}/g
 
 function tmpl(str, data) {
-
 	return str.replace(tmplReg, function (str, key) {
 		var value = data[key];
-
 		if (value === undefined)
 			value = "${"+key+"}";
-
 		return value;
 	});	
 }
